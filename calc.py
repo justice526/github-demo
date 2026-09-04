b62fad0f3d6189c51c842d8d195d34e2908728b6
baicai <3490831604@qq.com>
2026-09-04 12:02:19 +0800

    feat: add multiply and divide function for calculator

--- calc.py
+++ calc.py
@@ -1,9 +1,19 @@
 def add(a : int, b : int) -> int:
     return a + b
+
 def sub(a : int, b : int) -> int:
     return a - b
+
+def mul(a: int, b: int) -> int:
+    return a * b
+
+def div(a: int, b: int) -> float:
+    return a / b
+
 if __name__ == "__main__":
     a = eval(input())
     b = eval(input())
     print("加法：{}".format(add(a, b)))
     print("减法：{}".format(sub(a, b)))
+    print("乘法：{}".format(mul(a, b)))
+    print("除法：{}".format(div(a, b)))
